47714525391248ae3d6bf7b999b7636d6ed228df
hieu <anhhieuaxz@gmail.com>
2022-07-22 20:37:03 +0700

    hdhd

--- E-tool/src/reducers/TypeMapActiveReducer.js
+++ E-tool/src/reducers/TypeMapActiveReducer.js
@@ -6,7 +6,6 @@ const TypeMapActiveReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'CHANGER_TYPE_MAP_ACTIVE': {
       const listType = action
-      console.log(listType.mapActive)
       return { ...state, type: listType.mapActive }
     }
 
